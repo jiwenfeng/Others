909cf69565286dc00bf49f4e44aee0e9c4b9b7e5
jiwenfeng <490257094@qq.com>
2016-01-11 23:16:24 +0800

    add a script

---
+++ gcc_auto_install.sh
@@ -0,0 +1,64 @@
+#! /bin/sh
+
+# Download prerequisites needed by gcc and install.
+# Run this from the top level of the gcc source tree.
+# Run this as root
+
+apt-get update
+apt-get install build-essential manpages-dev || exit 1
+
+# Necessary to build GCC.
+M4=m4-1.4.17
+MPFR=mpfr-2.4.2
+GMP=gmp-4.3.2
+MPC=mpc-0.8.1
+
+DIR=dependence
+
+if [ ! -d $DIR ] ; then
+	mkdir $DIR
+	cd $DIR
+fi
+
+Install()
+{
+	echo "Now Install $2"
+	filename=${1##*/}
+	if [ ! -f $filename ] ; then
+		wget $1 || echo "Download $filename Failed"; exit 1
+	fi
+	if [ ! -d $2 ]; then
+		tar xvf $filename || exit 1
+	fi
+	cd $2
+	./configure --prefix=/usr/local/ && make -j4 && make install || echo "Install $2 Failed";exit 1
+	echo "$2 Installed Finish"
+	cd ..
+}
+
+# Install M4
+if [ ! -f /usr/local/bin/m4 -a ! -f /usr/bin/m4 ] ; then
+	Install http://ftp.gnu.org/gnu/m4/$M4.tar.xz $M4
+fi
+
+# Install MPFR
+if [ ! -f /usr/local/lib/libmpfr.a -a ! -f /usr/lib/libmpfr.a ] ; then
+	Install ftp://gcc.gnu.org/pub/gcc/infrastructure/$MPFR.tar.bz2 $MPFR
+fi
+
+# Install GMP
+if [ ! -f /usr/local/lib/libgmp.a -a ! -f /usr/lib/libgmp.a ]; then
+	Install ftp://gcc.gnu.org/pub/gcc/infrastructure/$GMP.tar.bz2 $GMP
+fi
+
+# Install MPC
+if [ ! -f /usr/local/lib/libmpc.a -a ! -f /usr/lib/libmpc.a ] ; then
+	Install ftp://gcc.gnu.org/pub/gcc/infrastructure/$MPC.tar.gz $MPC
+fi
+
+ldconfig
+
+echo "Now Install GCC"
+./configure --prefix=/usr/local/ --disable-multilib --enable-checking=release --enable-languages=c,c++ && make -j4 && make install && make clean|| exit 1
+rm -rf $DIR
+echo "All Installed Finish"
